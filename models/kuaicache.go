55a783a1e2cc6fc53cbeeee535fd7ce042dbc72e
ccwawamiya <101789019@qq.com>
2015-12-23 16:18:57 +0800

    '修改redis缓存'

--- models/kuaicache.go
+++ models/kuaicache.go
@@ -5,13 +5,23 @@ import (
 	"github.com/astaxie/beego/orm"
 	"github.com/fatih/structs"
 	"github.com/garyburd/redigo/redis"
+	"io"
+	"log"
+	"os"
 	"strings"
 )
+
+type Log struct {
+	*log.Logger
+}
 
 type Kuaicache struct {
 	gameKey string
 }
 
+var (
+	DebugLog = NewLog(os.Stderr)
+)
 var c redis.Conn
 
 var _prefix = ""
@@ -21,23 +31,30 @@ func init() {
 	if err == nil {
 		c = d
 	} else {
-		panic("no connect redis")
+		DebugLog.Println("no connect redis")
 	}
 	_prefix = beego.AppConfig.String("redisPrefix")
 }
 
 func (this *Kuaicache) GetGameCache(gameKey string) (bool, map[string]interface{}) {
-	info, err := redis.String(c.Do("HGET", _prefix+"game_id_", gameKey))
-	if err == nil {
-		status, gameinfo := this.GetGameCacheById(info)
-		if status {
-			return true, gameinfo
+	if c == nil {
+		goto Findmode
+	}
+	{
+		info, err := redis.String(c.Do("HGET", _prefix+"game_id_", gameKey))
+		if err == nil {
+			status, gameinfo := this.GetGameCacheById(info)
+			if status {
+				return true, gameinfo
+			}
+
 		}
 	}
+Findmode:
 	om := orm.NewOrm()
 	game := Game{Game_key: gameKey}
-	err = om.Read(&game, "Game_key")
-	if err != orm.ErrNoRows {
+	oerr := om.Read(&game, "Game_key")
+	if oerr != orm.ErrNoRows {
 		mapp := structs.Map(game)
 		return true, mapp
 	}
@@ -57,18 +74,24 @@ func (this *Kuaicache) GetGameCacheById(gameId string) (bool, map[string]interfa
 }
 
 func (this *Kuaicache) GetTokenCache(token string) (bool, map[string]interface{}) {
-	sinfo := make(map[string]interface{})
-	info, err := redis.StringMap(c.Do("HGETALL", _prefix+"oauthtoken:"+token))
-	if err == nil && len(info) > 0 {
-		for key, val := range info {
-			sinfo[strings.Title(key)] = val
-		}
-		return true, sinfo
+	if c == nil {
+		goto Findmode
 	}
+	{
+		sinfo := make(map[string]interface{})
+		info, err := redis.StringMap(c.Do("HGETALL", _prefix+"oauthtoken:"+token))
+		if err == nil && len(info) > 0 {
+			for key, val := range info {
+				sinfo[strings.Title(key)] = val
+			}
+			return true, sinfo
+		}
+	}
+Findmode:
 	om := orm.NewOrm()
 	oauth := Auth_code{Code: token}
-	err = om.Read(&oauth, "Code")
-	if err != orm.ErrNoRows {
+	oerr := om.Read(&oauth, "Code")
+	if oerr != orm.ErrNoRows {
 		mapp := structs.Map(oauth)
 		return true, mapp
 	}
@@ -76,21 +99,33 @@ func (this *Kuaicache) GetTokenCache(token string) (bool, map[string]interface{}
 }
 
 func (this *Kuaicache) GetUserCacheByPuid(puid int) (bool, map[string]interface{}) {
-	sinfo := make(map[string]interface{})
-	info, err := redis.StringMap(c.Do("HGETALL", _prefix+"thirdpuid:"+string(puid)))
-	if err == nil && len(info) > 0 {
-		for key, val := range info {
-			sinfo[strings.Title(key)] = val
-		}
-		return true, sinfo
+	if c == nil {
+		goto Findmode
 	}
-
+	{
+		sinfo := make(map[string]interface{})
+		info, err := redis.StringMap(c.Do("HGETALL", _prefix+"thirdpuid:"+string(puid)))
+		if err == nil && len(info) > 0 {
+			for key, val := range info {
+				sinfo[strings.Title(key)] = val
+			}
+			return true, sinfo
+		}
+	}
+Findmode:
 	om := orm.NewOrm()
 	users := Users{Puid: puid}
-	err = om.Read(&users, "Puid")
-	if err != orm.ErrNoRows {
+	oerr := om.Read(&users, "Puid")
+	if oerr != orm.ErrNoRows {
 		mapp := structs.Map(users)
 		return true, mapp
 	}
 	return false, nil
+}
+
+// set io.Writer to create a Logger.
+func NewLog(out io.Writer) *Log {
+	d := new(Log)
+	d.Logger = log.New(out, "[ORM]", 1e9)
+	return d
 }
